5e133593fcfcdaf47617c80507c6198d94e12030
sachin-myysports <sachin@myysports.com>
2024-12-15 22:43:14 +0530

    home route added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import userRouter from './routes/users';
 
 const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.use('/api/users', userRouter);
 
